d3054c266a6d62a5a7b6db4e07d30f1cf015459f
Matt Massicotte <matt.massicotte@gmail.com>
2011-10-08 11:23:39 -0400

    Fixed up the if statement to make use of the conditional codegen stuff.  There is still some duplication in there, but it's not terrible

--- compiler/ast/control/if_statement.cpp
+++ compiler/ast/control/if_statement.cpp
@@ -64,47 +64,15 @@ namespace ast
     
     llvm::Value* IfStatement::codegen(chime::code_generator& generator)
     {
-        llvm::Value*      conditionValue;
         llvm::Function*   function;
-        llvm::BasicBlock* thenBlock;
-        llvm::BasicBlock* elseBlock;
         llvm::BasicBlock* endBlock;
-        
-        // codegen the condition...
-        conditionValue = this->getCondition()->codegen(generator);
-        
-        // ... and wrap it up in a conditional test
-        conditionValue = generator.createCondition(conditionValue);
+        llvm::Value*      value;
         
         function = generator.builder()->GetInsertBlock()->getParent();
         
-        thenBlock = llvm::BasicBlock::Create(generator.get_context(), "if.then", function);
-        elseBlock = llvm::BasicBlock::Create(generator.get_context(), "if.else");
-        endBlock  = llvm::BasicBlock::Create(generator.get_context(), "if.end");
+        endBlock = llvm::BasicBlock::Create(generator.get_context(), "if.end");
         
-        generator.builder()->CreateCondBr(conditionValue, thenBlock, elseBlock);
-        
-        // setup the then block
-        generator.builder()->SetInsertPoint(thenBlock);
-        
-        this->getBody()->codegen(generator);
-        
-        // branch to the end, unless we've encountered another terminator
-        // in the process of codegen (like a return statement)
-        if (!generator.builder()->GetInsertBlock()->getTerminator())
-        {
-            generator.builder()->CreateBr(endBlock);
-        }
-        
-        function->getBasicBlockList().push_back(elseBlock);
-        generator.builder()->SetInsertPoint(elseBlock);
-        
-        // codegen the else block.  If there is no else, we just have it
-        // branch right to the end
-        if (this->getElse())
-        {
-            this->getElse()->codegen(generator);
-        }
+        value = this->codegenConditional(generator, endBlock);
         
         // guard against duplicate terminators again here
         if (!generator.builder()->GetInsertBlock()->getTerminator())
@@ -112,11 +80,9 @@ namespace ast
             generator.builder()->CreateBr(endBlock);
         }
         
-        elseBlock = generator.builder()->GetInsertBlock();
-        
         function->getBasicBlockList().push_back(endBlock);
         generator.builder()->SetInsertPoint(endBlock);
         
-        return NULL;
+        return value;
     }
 }
